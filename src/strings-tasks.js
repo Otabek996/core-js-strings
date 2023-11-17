37838d7d67472dbaf45183f7c6a83cf87056a107
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-11-17 19:34:09 +0500

    docs: solve the problem is palindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -319,8 +319,9 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const newStr = str.toLowerCase().replace(/[\W_]/g, '');
+  return newStr === newStr.split('').reverse().join('');
 }
 
 /**
